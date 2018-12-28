65337578285c29d1d61541546d8df43ac8a605af
root <twhtanghk@gmail.com>
2018-12-28 12:18:26 +0800

    extract routes into separate module

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,8 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import router from './router.vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
-Vue.use(VueRouter)
-const router = new VueRouter({
-  routes: [
-    {path: '/login', component: require('./login').default},
-    {path: '/reset', component: require('./reset').default},
-    {path: '/register', component: require('./register').default},
-    {path: '*', redirect: '/login'}
-  ]
-})
 
 new Vue({
   router,
---
+++ frontend/src/router.vue
@@ -0,0 +1,15 @@
+<script lang='coffee'>
+Vue = require('vue').default
+VueRouter = require('vue-router').default
+
+Vue.use(VueRouter)
+
+export default new VueRouter
+  routes: [
+    {path: '/user/register', component: require('./register').default}
+    {path: '/user/activate', component: require('./activate').default}
+    {path: '/user/login', component: require('./login').default}
+    {path: '/user/reset', component: require('./reset').default}
+    {path: '*', redirect: '/user/login'}
+  ]
+</script>
