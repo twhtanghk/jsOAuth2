af549ee720a38641ed8187a3b7f75ce5c7110fbe
root <twhtanghk@gmail.com>
2018-12-31 12:16:26 +0800

    add activate frontend

---
+++ frontend/src/activate.vue
@@ -0,0 +1,12 @@
+<script lang='coffee'>
+url = require 'url'
+{User} = require('./model').default
+
+export default
+  created: ->
+    {query} = url.parse window.location.toString(), true
+    User
+      .activate query.hash
+      .then =>
+        @$router.push path: '/user/login'
+</script>
