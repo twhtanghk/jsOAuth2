404c761cabcdd9f512f8a4be8db197a6539580d2
root <twhtanghk@gmail.com>
2018-12-31 11:56:51 +0800

    add font awesome svg packages

---
+++ frontend/src/font.vue
@@ -0,0 +1,12 @@
+<script lang='coffee'>
+Vue = require('vue').default
+{library} = require '@fortawesome/fontawesome-svg-core'
+{FontAwesomeIcon} = require '@fortawesome/vue-fontawesome'
+{faUserCircle, faQuestion, faSearch, faList, faUserShield, faUserLock} = require '@fortawesome/free-solid-svg-icons'
+library.add faUserCircle, faQuestion, faSearch, faList, faUserShield, faUserLock
+Vue.component 'font-awesome-icon', FontAwesomeIcon
+</script>
+
+<style lang='scss'>
+@import '~@fortawesome/fontawesome-svg-core/styles.css';
+</style>
